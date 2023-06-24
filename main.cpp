058535022f99d04e37c09551702732e64067e2be
Dinh Nguyen Anh Dung <dungdna2000@gmail.com>
2023-06-24 20:09:02 +0700

    Add unit test for class Circle

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include "circle.h"
 
 using namespace std;
 
+// Unit test for class Circle
+void testCircle() {
+	Circle c1(5.0);
+	c1.print();     // expected 25*3.1415 ~ xyz
+	Circle c2(6.0);
+	c2.print();
+}
+
+
 int main(int)
 {
-	cout << "Hello, world" << endl;
-	cout << "Hey, is it C or C++ ?" << endl;
+	//cout << "Hello, world" << endl;
+	//cout << "Hey, is it C or C++ ?" << endl;
+	testCircle();
 }
